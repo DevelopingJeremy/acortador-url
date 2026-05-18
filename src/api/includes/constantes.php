501db15d6d2e77b44c5b4f77f5d4b58fbd0535cf
DevelopingJeremy <quesadajeremy7@gmail.com>
2026-05-18 14:35:35 -0600

    a

--- src/api/includes/constantes.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-$dominio = 'http://localhost:8888/url-shortenner/';
-
-?>
--- src/api/includes/cors.php
+++
@@ -1,6 +0,0 @@
-<?php
-    header('Access-Control-Allow-Origin: http://localhost:8888'); // Permite el acceso desde cualquier origen
-    header('Access-Control-Allow-Headers: Content-Type');
-    header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
-    header('Content-Type: application/json')
-?>
